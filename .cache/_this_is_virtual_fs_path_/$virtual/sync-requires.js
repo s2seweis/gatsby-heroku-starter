9a66e70910fe12f0a5be44fb1db7aca76d4868cb
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-03-06 02:25:34 +0100

    latest

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,14 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/src/pages/about.js")),
+  "component---src-pages-contact-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/src/pages/contact.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/src/pages/index.js")),
+  "component---src-pages-projects-js": preferDefault(require("/Users/SWT/Desktop/testbackend/gatsby-heroku-starter/src/pages/projects.js"))
+}
+
