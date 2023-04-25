b36b7b02300eb1928f34f3855c6b6eeeadb1127f
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-04-25 11:38:07 +0200

    upload

--- src/pages/adobe.js
+++ src/pages/adobe.js
@@ -302,3 +302,95 @@
 // };
 
 // export default Adobe;
+
+
+
+// ###
+
+import React from "react"
+import { graphql } from "gatsby"
+import Title from "../components/Title"
+import Seo from "../components/Seo"
+
+
+
+const About = ({ data }) => {
+  console.log(data);
+  const {
+    strapiAbout: { title, image, info, stack },
+  } = data
+  return (
+    <>
+      <Seo title="About" />
+      <section className="about-page">
+
+
+        
+
+
+        <div 
+        
+        
+        // style={{display:"flex"}}
+        
+        className="section-center1 about-center">
+          <img src={image.localFile.publicURL} alt={title} className="about-img-svg" />
+
+
+
+         
+
+
+
+          <article className="about-text">
+            <Title title={title} />
+            <p>{info}</p>
+
+
+
+
+            <div className="about-stack">
+
+            <div className="abou-stack-center"
+
+            style={{display:"table", margin:"auto"}}
+
+            
+            
+            >
+
+
+              {stack.map(item => {
+                return <span key={item.id}>{item.title}</span>
+              })}
+            </div>
+
+              </div>
+
+
+          </article>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    strapiAbout {
+      title
+      info
+      stack {
+        id
+        title
+      }
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`
+
+export default About
