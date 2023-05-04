8f3dd02641ebcf9fabf5d9becc74986a72595762
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-05-04 12:56:14 +0200

    upload

--- src/pages/thankyou.js
+++ src/pages/thankyou.js
@@ -8,7 +8,8 @@ const thankyou = () => {
         <article style={{height:"200px"}} className="contact-form">
 
           <h2 style={{marginTop:"30px" }}>Thank You !!!</h2>
-          <h3>I got your email. Will contact you soon.</h3>
+          <p>Thank you for your E-Mail. </p>
+          <p>I will soon come to you back.</p>
           
         </article>
       </section>
